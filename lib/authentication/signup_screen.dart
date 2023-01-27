0a9ff99cbfe9f4ece2386ec71e65c41248045beb
Nomi <muhammadnumanlatif@gmail.com>
2023-01-27 14:56:58 +0500

    Signup Screen

--- lib/authentication/signup_screen.dart
+++ lib/authentication/signup_screen.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 
+import '../widgets/custom_textfield.dart';
+import '../widgets/cutom_button.dart';
+
 class SignupScreen extends StatefulWidget {
   const SignupScreen({Key? key}) : super(key: key);
 
@@ -8,8 +11,72 @@ class SignupScreen extends StatefulWidget {
 }
 
 class _SignupScreenState extends State<SignupScreen> {
+
+  TextEditingController? nameController = TextEditingController();
+  TextEditingController? emailController = TextEditingController();
+  TextEditingController? phoneController = TextEditingController();
+  TextEditingController? passwordController = TextEditingController();
+  @override
+  void dispose() {
+    nameController!.dispose();
+    emailController!.dispose();
+    phoneController!.dispose();
+    passwordController!.dispose();
+    super.dispose();
+  }
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      backgroundColor: Colors.black,
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            children: [
+             Padding(padding: EdgeInsets.all(20),
+             child:  Hero(
+                 tag: "logo",
+                 child: Image.asset("images/logo1.png")),
+             ),
+              SizedBox(height: 10,),
+             const Text("Resgister as a Driver",
+              style: TextStyle(
+                color: Colors.amber,
+                fontSize: 26,
+                fontWeight: FontWeight.bold,
+              ),
+              ),
+              CustomTextField(
+                  nameController: nameController,
+                label: "Name",
+                ),
+              CustomTextField(
+                nameController: emailController,
+                label: "Email",
+                textInputType: TextInputType.emailAddress,
+              ),
+              CustomTextField(
+                nameController: phoneController,
+                label: "Phone",
+                textInputType: TextInputType.phone,
+              ),
+              CustomTextField(
+                nameController: passwordController,
+                label: "Password",
+                isObsucreType: true,
+              ),
+
+              SizedBox(height: 20,),
+              //button
+              CustomButton(
+                text: "Create Account",
+                function: (){},
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
+

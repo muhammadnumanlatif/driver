c127a9f6a37f5ebb2d2e03f4cccddcbec3d671d5
Nomi <muhammadnumanlatif@gmail.com>
2023-01-31 14:20:37 +0500

    Firebase Connected

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,15 @@
 import 'package:driver/splashScreen/splash_screen.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:responsive_sizer/responsive_sizer.dart';
 
+import 'firebase_options.dart';
+
 void main() async{
   WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp( MyApp(
     child:ResponsiveSizer(
       builder:  (context, orientation, screenType) {
